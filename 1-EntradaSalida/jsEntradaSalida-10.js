b13a3bcfafa670b794c7eebf9333ad761e1be186
nExtremis <herera.gonzalo1994@gmail.com>
2022-02-04 11:36:12 -0300

    clase 02

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -10,11 +10,16 @@ function mostrarAumento()
 	let descuento;
 	let importeConDescuento;
 
+
+	//guardo el importe en la variable y lo convierto a entero
 	importe = parseInt(document.getElementById("txtIdImporte").value);
 
+	// calculo el descuento
 	descuento = importe * 0.25;
 
+	// calculo el importe final con el descuento aplicado
 	importeConDescuento = importe - descuento;
 
+	//muestro en el cuadro de texto el importe con el descuento
 	document.getElementById("txtIdResultado").value = importeConDescuento;
 }
